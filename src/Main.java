3d67d73081f2606ea58c606b62d4106d18185e4b
lifanova <veralifanovaa@gmail.com>
2021-03-10 16:34:50 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        
+        long currentSum = 1005678;
+        long addedSum = 1678;
+
+        long resultBonus = calculateBonus(addedSum);
+
+        currentSum += (addedSum + resultBonus);
+
+        System.out.printf("Итоговый бонус: %d\n", resultBonus);
+        System.out.printf("Количество денег на текущем счёте клиента: %d", currentSum);
+    }
+
+    public static long calculateBonus(long sum){
+        int limitSum = 1000;
+        int divider = 100;
+
+        if(sum > limitSum) {
+            return sum / divider;
+        }
+
+        return 0;
     }
 }
